3fc702d2340b398114815ce4f1f51a25a1ab1175
wonho <adler0311@gmail.com>
2019-11-01 17:58:08 +0900

    이미지 추가/ app.json 수정

--- Loading.js
+++ Loading.js
@@ -1,21 +1,32 @@
 import React from "react";
-import { StyleSheet, View, Text, StatusBar } from "react-native";
+import { StyleSheet, View, Text, StatusBar, Image } from "react-native";
 export default function Loading() {
   return (
     <View style={styles.container}>
       <StatusBar barStyle="default" />
-      <Text style={styles.text}>Getting the fucking weather</Text>
+      <View style={styles.imageContainer}>
+        <Image opacity={0.6} source={require("./assets/cloud.jpg")} />
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.loadingText}>Loading...</Text>
+        <Text style={styles.text}>Getting the weather</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    justifyContent: "center",
+    flex: 1
+  },
+  text: { fontSize: 30, color: "white" },
+  imageContainer: { flex: 3, justifyContent: "center" },
+  textContainer: {
     flex: 1,
     justifyContent: "flex-end",
     paddingHorizontal: 30,
-    paddingVertical: 100,
-    backgroundColor: "#FDF6AA"
+    paddingBottom: 100
   },
-  text: { fontSize: 30, color: "#2c2c2c" }
+  loadingText: { color: "white", marginBottom: 10, fontSize: 20 }
 });
